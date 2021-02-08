b67cf52347a2b9f07093628caccf7d360b7a3ab7
Shuai <guo@tfd.mw.tum.de>
2021-02-08 11:30:54 +0100

    Add kernel parameter

--- GaussianProcess/GaussianProcess.py
+++ GaussianProcess/GaussianProcess.py
@@ -5,15 +5,18 @@ class GaussianProcess:
     """A class that trains a Gaussian Process model
     to approximate functions"""
 
-    def __init__(self, n_restarts, optimizer, inital_point):
+    def __init__(self, n_restarts, optimizer, inital_point, kernel):
         """Initialize a Gaussian Process model
 
         Input
         ------
-        n_restarts: number of restarts of the local optimizer
-        optimizer: algorithm of local optimization
-        inital_point: user-specified starting point"""
+        n_restarts (int): number of restarts of the local optimizer
+        optimizer (string): algorithm of local optimization
+                   (see scipy.optimize.minimize methods)
+        inital_point (array): user-specified starting point
+        kernel (string): kernel type"""
 
         self.n_restarts = n_restarts
         self.optimizer = optimizer
         self.init_point = inital_point
+        self.kernel = kernel
