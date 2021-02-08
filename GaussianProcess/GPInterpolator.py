62dd58e5cc7fce9563db8b031b0723167c7f1810
Shuai <guo@tfd.mw.tum.de>
2021-02-08 23:00:37 +0100

    Compute NegLnlog

--- GaussianProcess/GPInterpolator.py
+++ GaussianProcess/GPInterpolator.py
@@ -23,4 +23,28 @@ class GPInterpolator(GaussianProcess):
         ------
         NegLnLike: Negative log-likelihood value"""
 
-        
+        theta = 10**theta    # Correlation length
+        n = self.X.shape[0]  # Number of training instances
+
+        if self.trend == 'Const':
+            F = np.ones((n,1))
+
+        # Construct correlation matrix
+        K = self.Corr(self.X, self.X, theta) + np.eye(n)*self.nugget
+        L = np.linalg.cholesky(K)
+
+        # Mean estimation
+        mu = (F.T @ (cho_solve((L, True), self.y))) / \
+            (F.T @ (cho_solve((L, True), F)))
+
+        # Variance estimation
+        SigmaSqr = (self.y-mu*F).T @ (cho_solve((L, True), self.y-mu*F)) / n
+
+        # Compute log-likelihood
+        LnDetK = 2*np.sum(np.log(np.abs(np.diag(L))))
+        LnLike = -(n/2)*np.log(SigmaSqr) - 0.5*LnDetK
+
+        # Update attributes
+        self.K, self.F, self.L, self.mu, self.SigmaSqr = K, F, L, mu, SigmaSqr
+
+        return -LnLike.flatten()
