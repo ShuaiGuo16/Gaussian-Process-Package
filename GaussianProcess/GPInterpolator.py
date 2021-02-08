c5273265a16d711b44427e2ee1cef5e43b38519e
Shuai <guo@tfd.mw.tum.de>
2021-02-08 12:19:01 +0100

    Init a interpolator class

---
+++ GaussianProcess/GPInterpolator.py
@@ -0,0 +1,15 @@
+# import all packages
+import numpy as np
+from scipy.optimize import minimize
+from scipy.optimize import Bounds
+from scipy.linalg import cho_solve
+from pyDOE import lhs
+from GaussianProcess import GaussianProcess
+
+class GPInterpolator(GaussianProcess):
+    """A class that trains a Gaussian Process model
+    to interpolate functions"""
+    
+    def __init__(self):
+
+    def Neglikelihood(self, theta):
