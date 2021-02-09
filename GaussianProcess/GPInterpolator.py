fefa5a36ba95a0860810a72ebd2ad3aff3ec2250
Shuai <guo@tfd.mw.tum.de>
2021-02-09 12:06:40 +0100

    Fix flatten bug in score method

--- GaussianProcess/GPInterpolator.py
+++ GaussianProcess/GPInterpolator.py
@@ -129,13 +129,13 @@ class GPInterpolator(GaussianProcess):
         Input
         -----
         X_test (array): test set, shape (n_samples, n_features)
-        y_test (array): test labels, shape (n_samples, )
+        y_test (array): test labels
 
         Output
         ------
         RMSE: the root mean square error"""
 
         y_pred, SSqr = self.predict(X_test)
-        RMSE = np.sqrt(np.mean((y_pred-y_test)**2))
+        RMSE = np.sqrt(np.mean((y_pred-y_test.flatten())**2))
 
         return RMSE
