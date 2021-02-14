Correct mean calculation

GaussianProcess/GPInterpolator.py
@@ -57,8 +57,8 @@ class GPInterpolator(GaussianProcess):
         L = np.linalg.cholesky(K)
 
         # Mean estimation
-        mu = np.linalg.lstsq(F.T @ (cho_solve((L, True), F)),
-        F.T @ (cho_solve((L, True), self.y))).reshape(-1, 1)
+        mu = np.linalg.solve(F.T @ (cho_solve((L, True), F)),
+        F.T @ (cho_solve((L, True), self.y)))
         # mu = (F.T @ (cho_solve((L, True), self.y))) / \
             # (F.T @ (cho_solve((L, True), F)))
 